31616096baba56685b5b4dc5ee60df947aa0e899
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-01-17 18:08:30 +0300

    Time: 1686 ms (5.03%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,10 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        diction = {}
-        for i,x in enumerate(names):
-            diction[heights[i]] = x
         l = 0
         while l < len(names):
             for i in range(len(names)-(l+1)):
                 if heights[i] < heights[i+1]:
                     heights[i],heights[i+1] = heights[i+1],heights[i]
+                    names[i],names[i+1] = names[i+1],names[i]
             l+=1
-        for i in range(len(names)):
-            names[i] = diction[heights[i]]
         return names
