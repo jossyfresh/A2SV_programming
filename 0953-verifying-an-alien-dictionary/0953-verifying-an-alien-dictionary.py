8d14d840770bd4933bfab17c94a261169c3d712d
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-02-03 19:12:22 +0300

    Time: 43 ms (49.47%), Space: 14 MB (31.45%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -7,12 +7,14 @@ class Solution:
             dict[word] = index
         while left < len_words-1:
             min_words = min(len(words[left]),len(words[left+1]))
+            letter1 = words[left]
+            letter2 = words[left+1]
             for i in range(min_words):
-                if dict[words[left][i]] > dict[words[left+1][i]]:
+                if dict[letter1[i]] > dict[letter2[i]]:
                     return False
-                elif dict[words[left][i]] < dict[words[left+1][i]]:
+                elif dict[letter1[i]] < dict[letter2[i]]:
                     break
-                if len(words[left])-1 > i and len(words[left+1])-1 == i:
+                if len(letter1)-1 > i and len(letter2)-1 == i:
                     return False
             left+=1
         return True
