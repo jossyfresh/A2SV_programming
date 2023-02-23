afd2bcfd513580523acf537ae0d963618ff43c81
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-02-23 14:20:44 +0300

    added longest-turbulent-subarray.python

---
+++ longest-turbulent-subarray.py
@@ -0,0 +1,44 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        r = 1
+        l = 0
+        max_ = 0
+        prev = arr[0]
+        if len(arr) == 1:
+            return 1
+        up = True
+        while r < len(arr):
+            if prev > arr[r]:
+                up = True
+                break
+            elif prev < arr[r]:
+                up = False
+                break
+            prev = arr[r]
+            r+=1
+            
+        l = r-1   
+        while r < len(arr):
+            cur = arr[r]
+            if up and prev > cur:
+                prev = cur
+                r+=1
+                up = False
+            elif not up and prev < cur:
+                prev = cur
+                r+=1
+                up = True
+            else:
+                max_ = max(max_,r-l)
+                while r < len(arr):
+                    if prev > arr[r]:
+                        up = True
+                        break
+                    elif prev < arr[r]:
+                        up = False
+                        break
+                    prev = arr[r]
+                    r+=1
+                l = r-1
+        max_=max(max_,(r-l))
+        return max_
