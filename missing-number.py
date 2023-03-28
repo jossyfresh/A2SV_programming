fef771cc79294ccaf046eb0a4ffeee5122dd7fb6
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-03-28 19:10:46 +0300

    added missing-number.python

--- missing-number.py
+++ missing-number.py
@@ -1,5 +1,16 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        for x in range(len(nums)+1):
-            if x not in nums:
-                return x
+    def missingNumber(self, arr: List[int]) -> int:
+        i = 0
+        while i < len(arr):
+            correct_position = arr[i]
+            if correct_position < len(arr)-1 and arr[correct_position] != arr[i]:
+                arr[correct_position], arr[i] = arr[i], arr[correct_position]
+            else:
+                i += 1
+        l = 0
+        for x in arr:
+            if x == l:
+                l+=1
+            else:
+                return l
+        return l
