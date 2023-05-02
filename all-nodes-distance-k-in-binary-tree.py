553675793e40e5e50bd7aafdd82e95017bb4da3e
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-05-02 17:21:57 +0300

    added all-nodes-distance-k-in-binary-tree.python

---
+++ all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        ans = []
+        def count(node,d):
+            if not node:
+                return 
+            if d == k:
+                ans.append(node.val)
+            count(node.left,d+1)
+            count(node.right,d+1)
+        l = 0
+        def tr(node):
+            if not node:
+                return 
+            if node == target:
+                count(node,l)
+                return 1
+            x = tr(node.left)
+            y = tr(node.right)
+            if x != None:
+                node.left = None
+                count(node,x)
+                return x+1
+            elif y!= None:
+                node.right = None
+                count(node,y)
+                return y+1                
+        tr(root)
+        return ans
