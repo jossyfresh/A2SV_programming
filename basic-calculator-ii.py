958a96c689863688c8b9f50343de065e17cc2be0
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-10-18 16:18:09 +0300

    added basic-calculator-ii.python

---
+++ basic-calculator-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack, cur, op = [], 0, '+'
+        for c in s + '+':
+            if c == " ":
+                continue
+            elif c.isdigit():
+                cur = (cur * 10) + int(c)
+            else:
+                if op == '-':
+                    stack.append(-cur)
+                elif op == '+':
+                    stack.append(cur)
+                elif op == '*':
+                    stack.append(stack.pop() * cur)
+                elif op == '/':
+                    stack.append(int(stack.pop() / cur))
+                cur = 0
+                op = c
+
+        return sum(stack)
