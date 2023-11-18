5f08ce31bd88981a398300939f067c7b01653cbd
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-11-18 19:46:28 +0300

    Added solution for Longest Common Prefix

---
+++ A2SV_programming/longest-common-prefix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        check = strs[0]
+        for word in strs:
+            if len(check)>len(word):
+                check = word                    
+        for x in strs:
+            size = min(len(x),len(check))
+            for i in range(size):
+                if x[i]==check[i]:
+                    continue
+                else:
+                    check = check[0:i]
+                    break
+                check = check[0:i]
+        return check
+            
+            
+        
