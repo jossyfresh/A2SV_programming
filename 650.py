01a0853eb1e2c33ae264222e0a262f27fccd6666
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2023-12-02 03:29:22 +0300

    added 650.py

---
+++ 650.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        memo = {}
+        if n == 1:
+            return 0
+        def dp(i,prev):
+            if i >= n:
+                if i == n:
+                    return 1
+                else:
+                    return float('inf')
+            x = dp(i+prev,prev) + 1
+            y = dp(i+prev,i+prev) + 2 
+            return min(x,y)
+        return dp(1,1)
+
+            
+
+
+
+        
